5ff3e8816d5f764972d799f35f41b187a1e874db
Local Host Tech <contacto@localhost.com.mx>
2015-09-18 18:32:38 -0500

    Método Ejecutar Creado

--- src/BaseDeDatos.java
+++ src/BaseDeDatos.java
@@ -1,3 +1,12 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +18,36 @@
  * @author Javier
  */
 public class BaseDeDatos {
+    
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resulSet;
+    
+    public BaseDeDatos(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/test");
+            statement = connection.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void ejecutar(String sql ){
+        try {
+            statement.execute(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String[] args){
+        BaseDeDatos bd = new BaseDeDatos();
+        bd.ejecutar("CREATE TABLE `test`.`TABLA_01` ( "
+                + "`CODIGO` INT NOT NULL AUTO_INCREMENT , "
+                + "`NOMBRE` VARCHAR(50) NOT NULL , "
+                + "`POSICION` VARCHAR(50) NOT NULL , "
+                + "`SALARIO` INT NOT NULL , "
+                + "PRIMARY KEY (`CODIGO`) )");
+    }
     
 }
